8a4acfc9de4044bb9bccf4e07718a41a62dee586
Hirahoshi <kenolymedeiros@hotmail.com>
2026-07-19 18:47:32 -0300

    implementação da classe Personagem e métodos básicos

---
+++ orientacao_objetos/Personagens.js
@@ -0,0 +1,32 @@
+/**
+ * Exercício 2: Orientação a Objetos
+ * Foco: Classes, construtor, atributos públicos e métodos.
+ */
+
+class Personagem {
+    constructor(nome, hp, ataque, defesa) {
+      this.nome = nome;
+      this.hp = hp; // Atributo ainda público (vulnerável)
+      this.ataque = ataque;
+      this.defesa = defesa;
+    }
+  
+    receberDano(danoBruto) {
+      const danoReal = danoBruto - this.defesa;
+      if (danoReal > 0) {
+        this.hp -= danoReal;
+        if (this.hp < 0) this.hp = 0;
+        console.log(`${this.nome} sofreu ${danoReal} de dano. HP: ${this.hp}`);
+      }
+    }
+  
+    atacar(alvo) {
+      console.log(`${this.nome} ataca ${alvo.nome}!`);
+      alvo.receberDano(this.ataque);
+    }
+  }
+  
+  // Teste
+  const heroi = new Personagem("Arthur", 100, 20, 10);
+  const inimigo = new Personagem("Goblin", 40, 15, 2);
+  heroi.atacar(inimigo);
