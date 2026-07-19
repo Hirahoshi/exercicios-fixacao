5f6c618ebf6ff50f8fe77b71da4a31308db81569
Hirahoshi <kenolymedeiros@hotmail.com>
2026-07-19 18:49:17 -0300

    aplicação de encapsulamento no atributo HP e criação de getters

---
+++ visibilidade_encapsulamento/PersonagemSeguro.js
@@ -0,0 +1,34 @@
+/**
+ * Exercício 3: Visibilidade e Encapsulamento
+ * Foco: Atributos privados (#) e métodos de acesso (getters).
+ */
+
+class PersonagemSeguro {
+    #hp; // Agora é privado
+  
+    constructor(nome, hpMaximo, ataque, defesa) {
+      this.nome = nome;
+      this.#hp = hpMaximo;
+      this.ataque = ataque;
+      this.defesa = defesa;
+    }
+  
+    // Permite ler o HP de fora, mas não alterar
+    get hp() {
+      return this.#hp;
+    }
+  
+    receberDano(danoBruto) {
+      const danoReal = danoBruto - this.defesa;
+      if (danoReal > 0) {
+        this.#hp -= danoReal;
+        if (this.#hp < 0) this.#hp = 0;
+        console.log(`${this.nome} sofreu ${danoReal} de dano. HP Restante: ${this.#hp}`);
+      }
+    }
+  }
+  
+  // Teste
+  const cavaleiro = new PersonagemSeguro("Lancelot", 150, 25, 15);
+  cavaleiro.receberDano(30);
+  // cavaleiro.#hp = 9999; // ERRO: O encapsulamento impede essa trapaça.
